4692f555aefc679c9365db8baf18a61184a09230
R3dHeadJ1n <ann228sup3r@gmail.com>
2026-02-13 13:12:51 +0700

    fixed db.js

--- backend/db.js
+++ backend/db.js
@@ -11,8 +11,4 @@ module.exports = {
   query: (text, params) => pool.query(text, params),
   pool,
 };
-<<<<<<< HEAD
-/* added comment for main branch*/
-=======
-/* comment for predprod push */
->>>>>>> main_predprod
+
