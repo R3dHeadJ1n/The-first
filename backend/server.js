aafb718f90496ce0f94b9d92f556408448e651be
R3dHeadJ1n <ann228sup3r@gmail.com>
2026-02-25 12:51:23 +0700

    loyverse integration v0.79

--- backend/server.js
+++ backend/server.js
@@ -2365,7 +2365,7 @@ async function loadMenuItemsFromDb() {
     }
 }
 
-// Load ALL menu items (no is_public filter) — for admin onlyy
+// Load ALL menu items (no is_public filter) — for admin only
 async function loadAllMenuItemsFromDb() {
     try {
         const result = await db.query(
